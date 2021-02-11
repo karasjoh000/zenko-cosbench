a4bd65a80171219e775fd98119a8a3edd57e330f
John Karasev <john.karasev@intel.com>
2021-02-11 10:10:44 -0800

    temp fix for https://github.com/intel-cloud/cosbench/issues/320. However, hashchecks do not work see: https://github.com/intel-cloud/cosbench/issues/406

--- images/build.sh
+++ images/build.sh
@@ -4,14 +4,16 @@
 
 BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 VCS_REF=$(git rev-parse --verify HEAD)
-VERSION=0.0.6
+VERSION=0.0.1
 
 docker build \
-        -t "zenko/zenko-cosbench:${VERSION}" \
+        -t "jkarasev/cosbench:${VERSION}" \
         --build-arg "COSBENCH_VERSION=${COSBENCH_VERSION}" \
         --build-arg "BUILD_DATE=${BUILD_DATE}" \
         --build-arg "VCS_REF=${VCS_REF}" \
         --build-arg "VERSION=${VERSION}" \
+        --build-arg http_proxy=$http_proxy \
+        --build-arg https_proxy=$http_proxy \
         .
 
-echo "Built zenko-cosbench:${VERSION}"
+echo "jkarasev/cosbench:${VERSION}"
